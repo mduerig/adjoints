80e11a2ac9610809f7432c68a5b286a7a13650f5
Michael Dürig <michid@gmail.com>
2019-06-05 17:11:23 +0200

    unit/counit from natural hom-set isomorphism

--- src/Adjoint.hs
+++ src/Adjoint.hs
@@ -8,12 +8,19 @@ class (Functor l, Functor r) => Adjoint l r where
     unit   :: d -> r (l d)
     counit :: l (r c) -> c
 
--- Natural isomorphism of hom-sets
+-- Natural isomorphism of hom-sets from unit and counit
 phiLeft :: Adjoint l r => (l d -> c) -> (d -> r c)
 phiLeft f = fmap f . unit
-    
+
 phiRight :: Adjoint l r => (d -> r c) -> (l d -> c)
 phiRight f = counit . fmap f
+
+-- Unit and counit from natural isomorphism of hom-sets
+unit' :: Adjoint l r => d -> r (l d)
+unit' = phiLeft id
+
+counit' :: Adjoint l r => l (r c) -> c
+counit' = phiRight id
 
 -- Triangular identities
 triLeft :: Adjoint l r => (r (l d) -> l (r (l d))) -> (d -> l d)
