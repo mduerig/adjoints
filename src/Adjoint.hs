2a7049128a463c48c57dd50a727c3ed3cc7acfa6
Michael Dürig <michid@gmail.com>
2019-06-05 18:19:35 +0200

    implemented phiLeft/phiRight in terms of unit/counit and vice versa

--- src/Adjoint.hs
+++ src/Adjoint.hs
@@ -5,22 +5,20 @@ module Adjoint
     ) where
 
 class (Functor l, Functor r) => Adjoint l r where
-    unit   :: d -> r (l d)
+    
+    -- Unit and counit from natural isomorphism of hom-sets
+    unit :: d -> r (l d)
+    unit = phiLeft id
+
     counit :: l (r c) -> c
+    counit = phiRight id
 
--- Natural isomorphism of hom-sets from unit and counit
-phiLeft :: Adjoint l r => (l d -> c) -> (d -> r c)
-phiLeft f = fmap f . unit
+    -- Natural isomorphism of hom-sets from unit and counit
+    phiLeft :: (l d -> c) -> (d -> r c)
+    phiLeft f = fmap f . unit
 
-phiRight :: Adjoint l r => (d -> r c) -> (l d -> c)
-phiRight f = counit . fmap f
-
--- Unit and counit from natural isomorphism of hom-sets
-unit' :: Adjoint l r => d -> r (l d)
-unit' = phiLeft id
-
-counit' :: Adjoint l r => l (r c) -> c
-counit' = phiRight id
+    phiRight :: (d -> r c) -> (l d -> c)
+    phiRight f = counit . fmap f
 
 -- Triangular identities
 triLeft :: Adjoint l r => (r (l d) -> l (r (l d))) -> (d -> l d)
@@ -28,7 +26,6 @@ triLeft f = counit . f . unit
 
 triRight :: Adjoint l r => (c -> r c) -> (l (r c) -> r (l (r c)))
 triRight f = unit . f . counit
-
 
 adjointMain :: IO ()
 adjointMain = putStrLn "adjointMain"
